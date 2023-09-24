8db1c61fec07d0e38e4085a6cfc5525dfbd8a331
WESLEY JOSE SANTOS <53940899+WesleyJoseSantos@users.noreply.github.com>
2023-09-24 04:50:11 -0300

    feat: implement BinarySerializer

--- CiotNet.Serializer/Infrastructure/BinarySerializer.cs
+++ CiotNet.Serializer/Infrastructure/BinarySerializer.cs
@@ -1,10 +1,12 @@
 ﻿using CiotNet.Serializer.Domain.Interfaces;
 using System;
+using System.Collections.Generic;
 using System.Reflection;
+using System.Text;
 
 namespace CiotNet.Serializer.Infrastructure
 {
-    public class BinarySerializer : IBinarySerializer
+    public class BinarySerializer : ISerializer
     {
         public int endIdx = 0;
 
@@ -16,7 +18,7 @@ namespace CiotNet.Serializer.Infrastructure
 
             foreach (var prop in type.GetProperties())
             {
-                var val = DeserializeValue(prop.PropertyType, data, ref idx);
+                var val = DeserializeValue(prop.PropertyType, data, ref idx, prop);
                 prop.SetValue(obj, val);
             }
 
@@ -25,12 +27,56 @@ namespace CiotNet.Serializer.Infrastructure
             return (T)obj;
         }
 
-        public byte[] Serialize(object data)
+        public byte[] Serialize<T>(T data)
         {
-            return new byte[] { (byte)data };
+            var bytes = new List<byte>();
+            var type = data.GetType();
+
+            foreach (var prop in type.GetProperties())
+            {
+                dynamic value = prop.GetValue(data);
+                var result = SerializeValue(value.GetType(), value, prop);
+                bytes.AddRange(result);
+            }
+
+            return bytes.ToArray();
         }
 
-        private dynamic DeserializeValue(Type type, byte[] data, ref int idx)
+        private byte[] SerializeValue(Type type, dynamic value, PropertyInfo prop = null)
+        {
+            if (type.IsEnum || type == typeof(byte))
+            {
+                byte @byte = (byte)value;
+                return new byte[] { @byte };
+            }
+            else if (type == typeof(string))
+            {
+                byte[] bytes = Encoding.ASCII.GetBytes(value + '\0');
+                return bytes;
+            }
+            else if (type.IsArray)
+            {
+                var size = (SizeAttribute)Attribute.GetCustomAttribute(prop, typeof(SizeAttribute));
+                Array array = (Array)value;
+                var bytes = new List<byte>();
+                for (int i = 0; i < size.Value; i++)
+                {
+                    var item = array.GetValue(i);
+                    bytes.AddRange(SerializeValue(item.GetType(), item));
+                }
+                return bytes.ToArray();
+            }
+            else if (type.IsClass)
+            {
+                return Serialize(value);
+            }
+            else
+            {
+                return BitConverter.GetBytes(value);
+            }
+        }
+
+        private dynamic DeserializeValue(Type type, byte[] data, ref int idx, PropertyInfo prop = null)
         {
             if(type.IsEnum)
             {
@@ -109,6 +155,17 @@ namespace CiotNet.Serializer.Infrastructure
                 return text;
             }
 
+            if (type.IsArray)
+            {
+                var size = (SizeAttribute)Attribute.GetCustomAttribute(prop, typeof(SizeAttribute));
+                dynamic arr = Array.CreateInstance(type.GetElementType(), size.Value);
+                for (int i = 0; i < size.Value; i++)
+                {
+                    arr[i] = DeserializeValue(type.GetElementType(), data, ref idx);
+                }
+                return arr;
+            }
+
             if (type.IsClass)
             {
                 MethodInfo method = typeof(BinarySerializer).GetMethod("Deserialize").MakeGenericMethod(type);
@@ -119,6 +176,5 @@ namespace CiotNet.Serializer.Infrastructure
 
             return 0;
         }
-
     }
 }
